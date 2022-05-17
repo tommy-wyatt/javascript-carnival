86a2b84767a03f10e6df155f69ec7ebecae150e5
Thomas Wyatt <thomaswyattdev@gmail.com>
2022-05-17 12:17:23 +1200

    Whack a mole! (mole styling still needed)

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,39 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+//find a way to refrence to all the cells in the table
+let boardArr = document.getElementsByTagName('TD')
+console.log(boardArr)
+
+//create a function that uses the cells and puts a mole into a random
+//this function needs to use the stored cell information in boardArr
+//needs to use the mole image
+
+function randomMole() {
+    let randomIndex = Math.floor(Math.random() * 24)
+    let randCell = boardArr[randomIndex]
+    let mole = document.createElement('img')
+    mole.classList.add('mole')
+    mole.src = 'mole.PNG'
+    mole.id = 'whack'
+    mole.onclick = whackedMole
+    randCell.appendChild(mole)
+}
+
+randomMole()
+
+//function for when clicking on the mole uses the above function
+//when mole is clicked should move to another cell
+//should play audio
+function whackedMole() {
+    let mole = document.getElementById('whack')
+    mole.remove()
+    randomMole()
+    let audio = new Audio()
+    audio.src = 'whack-audio.wav'
+    audio.play()
+    console.log('mole whacked!')
+}
